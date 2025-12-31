8e7093eee04f3ca0e888a99b531f2712dd2bbba9
juanaxyz <inijuana@gmail.com>
2025-12-31 08:56:52 +0800

    push scripts and workflows

---
+++ scripts/script-mjs12.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# ~/scripts/script-mjs12.sh
+
+echo "====== Scripts Start ======"
+
+# check requirement
+if [-z "$var" ]; then 
+	echo "Error: $var not found... exiting"
+	exit 1
+else 
+	echo "variable $var found"
+fi
